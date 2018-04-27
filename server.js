b14dffbc6db0347f5e2f238afa6c2f682fd1c5b2
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2018-04-27 20:08:49 +0300

    Add dynamic url to server.js or deleted img

--- server.js
+++ server.js
@@ -21,4 +21,9 @@ app.use(function(req, res, next){
     res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!');
 });
 
-var server = app.listen(3000);
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
