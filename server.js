2a48696f16e6814a85166b0c68c867d1cf5bc3da
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2018-05-08 09:34:40 +0300

    Change files.

--- server.js
+++ server.js
@@ -7,18 +7,14 @@ app.set('views', './views');
 app.use('/static', express.static("assets"));
 
 app.get('/', function(req, res){
-    res.render('content');
+    res.render('index');
 });
 
 app.get('/auth/google', function(req, res){
-    res.render('dynamic', {
-        name:'My dynamic page!',
-        url: "http://www.google.com"
+    res.render('log', {
+        login: req.query.login,
+        password: req.query.password
     });
-});
-
-app.use(function(req, res, next){
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!');
 });
 
 var server = app.listen(3000, 'localhost', function() {
@@ -26,4 +22,8 @@ var server = app.listen(3000, 'localhost', function() {
     var port = server.address().port;
 
     console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
+
+app.use(function(req, res, next){
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!');
 });
